f8cc6fababdfdf7e0ec72802d3019df3ccd5e50b
Grant Currey <grant.currey@wotifgroup.com>
2013-08-08 13:59:22 +1000

    externalized gradle credntials and updated grails binding updater to not depend on grails-core libraries

--- src/groovy/grails/plugin/cucumberjson/GrailsEndpointBindingUpdater.groovy
+++ src/groovy/grails/plugin/cucumberjson/GrailsEndpointBindingUpdater.groovy
@@ -1,8 +1,6 @@
 package grails.plugin.cucumberjson
 
 import com.wotifgroup.cucumber.json.EndpointBindingUpdater
-import grails.util.Environment
-
 /**
  * User: gcurrey
  * Date: 18/06/12
@@ -20,14 +18,14 @@ class GrailsEndpointBindingUpdater extends EndpointBindingUpdater {
     void initialize() {
         super.initialize()
 
-        def configObject = new ConfigSlurper(Environment.TEST.name).parse(new File(CONFIG_PATH).toURL())
+        def configObject = new ConfigSlurper("TEST").parse(new File(CONFIG_PATH).toURL())
         def urlBase = configObject.grails.serverURL ?: System.getProperty("grails.testing.functional.baseUrl")
 
         setBaseUrl(urlBase)
         setJsonResourceLoaderBaseDir("test/cucumber/json")
     }
 
-    public static String isGrailsApplication(){
+    public static def isGrailsApplication(){
         try{
             Class.forName("grails.util.Environment")
             return new File(CONFIG_PATH).exists()
